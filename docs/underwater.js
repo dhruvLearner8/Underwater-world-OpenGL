5a686f31717513e69320895922e161383e91f523
Dhruv <dhruvmpatel096@gmail.com>
2026-09-12 19:53:10 -0600

    Revert "Fix two real bugs surfaced by the web build, both applicable everywhere"
    This reverts commit 3a59965f0234b3e0d155a253f0876bbb354aecbe.

--- docs/underwater.js
+++ docs/underwater.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpk7b9l1r1.js
+// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmphc082qbc.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "Resources", true, true);
 
   })();
 
-// end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpk7b9l1r1.js
-// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpqc3hoaq2.js
+// end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmphc082qbc.js
+// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmp2v8eda0g.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpqc3hoaq2.js
-// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpop2ad7a8.js
+  // end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmp2v8eda0g.js
+// include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpxnrkb26l.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpop2ad7a8.js
+  // end include: /var/folders/n7/942vk_ys38bdq2l_kx_lzjfr0000gn/T/tmpxnrkb26l.js
 
 
 // Sometimes an existing Module object exists with properties
